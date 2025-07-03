37745e5d8ff9958e27a8e63fc233e4b286cb09db
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-07-03 11:33:57 +0800

    add comments to 88 easy

--- easy/88_merge_sorted_array.py
+++ easy/88_merge_sorted_array.py
@@ -7,10 +7,11 @@ def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
   
   # merge in reverse order to fill nums1 from the end (end with 0)
   while index_2 >= 0:
+    # add from nums1 if it's larger than nums2 or if nums1 is exhausted
     if index_1 >= 0 and nums1[index_1] > nums2[index_2]:
       nums1[index_merge] = nums1[index_1]
       index_1 -= 1
-    else:
+    else: # add from nums2 if it's larger than nums1 or if nums2 is exhausted
       nums1[index_merge] = nums2[index_2]
       index_2 -= 1
     index_merge -= 1
