4d48acdf707660d6c425fe2149249b9d60494959
Teo Zheng Yang <teozhengyang15@gmail.com>
2025-10-08 14:04:42 +0800

    feat: add solution to "Append Characters to String to Make Subsequence"

---
+++ arrayhashingstrings/2486.py
@@ -0,0 +1,17 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        s_pointer = 0
+        t_pointer = 0
+
+        # loop through both strings
+        while s_pointer < len(s) and t_pointer < len(t):
+            # advance both pointers if match
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+                t_pointer += 1
+            # advance s_pointer if no match
+            else:
+                s_pointer += 1
+        
+        # return the number of unmatched characters in t
+        return len(t) - t_pointer
